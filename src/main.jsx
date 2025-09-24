4f892dccdc3b0a1f8424707715b0d8030d522879
AmirHossein <khazaei.amir110@gmail.com>
2025-09-24 16:41:07 +0330

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+export function App() {
+    return (
+        <StrictMode>
+            <h1>Hello World!</h1>
+        </StrictMode>
+    )
+}
+
+const root = document.getElementById("root")
+
+createRoot(root).render(
+  <App />
 )
