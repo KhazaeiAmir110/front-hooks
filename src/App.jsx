6c617636dd8ef93c8f66ba93bf92f7a0fe4c5ef9
AmirHossein <khazaei.amir110@gmail.com>
2025-10-10 08:07:21 +0330

    start project my course list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import "./css/App.css"
+
 export function App() {
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <h1>My courses (3)</h1>
+            <div className="course-list">
+                <div className="course-item">
+                    <div className="course-item__img">
+                        <img src="../public/images/img1.jpg" alt="" />
+                    </div>
+                    <div className="course-item__detail">
+                        <div className="course-item__body">
+                            <p className="title">React.js Course</p>
+                            <p className="desc">The Ultimate React and Redux Course</p>
+
+                            <div className="rate">4</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
